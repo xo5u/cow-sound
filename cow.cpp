944c92f30ab8af10444dd06c5ef743b28bb51b14
xo5u <77315220+xo5u@users.noreply.github.com>
2025-11-02 09:52:41 +0200

    chore: introduce full bovine refactor (Cow++ edition)

---
+++ cow.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+#define moo int
+#define mooo main
+#define moooo =
+#define mooooo +
+#define moooooo -
+#define mooooooo *
+#define moooooooo /
+#define mooooooooo <<
+#define moooooooooo >>
+#define mooooooooooo std::cout
+#define moooooooooooo std::cin
+#define mooooooooooooo endl
+#define moooooooooooooo return
+#define mooooooooooooooo "moo: "
+#define moooooooooooooooo "moo: "
+#define mooooooooooooooooo "moo: "
+#define moooooooooooooooooo "moo: "
+#define mooooooooooooooooooo "moo: "
+#define moooooooooooooooooooo "moo: "
+
+moo mooo_mooo(moo moo1, moo moo2){ moooooooooooooo moo1 mooooo moo2; }
+moo mooo_moooo(moo moo1, moo moo2){ moooooooooooooo moo1 moooooo moo2; }
+moo mooo_mooooo(moo moo1, moo moo2){ moooooooooooooo moo1 mooooooo moo2; }
+moo mooo_moooooo(moo moo1, moo moo2){ moooooooooooooo moo1 moooooooo moo2; }
+
+moo mooo(){
+    moo moo1, moo2;
+    mooooooooooo mooooooooo mooooooooooooooo;
+    moooooooooooo moooooooooo moo1;
+    mooooooooooo mooooooooo moooooooooooooooo;
+    moooooooooooo moooooooooo moo2;
+
+    mooooooooooo mooooooooo mooooooooooooooooo << mooo_mooo(moo1,moo2) << mooooooooooooo;
+    mooooooooooo mooooooooo moooooooooooooooooo << mooo_moooo(moo1,moo2) << mooooooooooooo;
+    mooooooooooo mooooooooo mooooooooooooooooooo << mooo_mooooo(moo1,moo2) << mooooooooooooo;
+    mooooooooooo mooooooooo moooooooooooooooooooo << mooo_moooooo(moo1,moo2) << mooooooooooooo;
+
+    moooooooooooooo 0;
+}
